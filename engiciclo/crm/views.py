a067f1f42859e746af170b5477779478d77c5dc2
Joao Trindade <trindade.joao@gmail.com>
2010-10-25 02:04:04 +0100

    Fixed the templates

--- engiciclo/crm/views.py
+++ engiciclo/crm/views.py
@@ -1,3 +1,4 @@
+import pdb
 # Create your views here.
 from django.shortcuts import render_to_response
 
@@ -6,7 +7,8 @@ from crm.models import Empresa
 
 def index(request):
     empresas_list = Empresa.objects.all().order_by('nome')
-    return render_to_response('empresa/empresa_lista.html', {'empresas': empresas_list})
+    return render_to_response('empresa/empresa_lista.html', {'empresas_list': empresas_list})
 
 def detail(request, empresa_id):
-    return HttpResponse("You're looking at poll %s." % empresa_id)
+    empresa = Empresa.objects.filter('id'=empresa_id)
+    return render_to_response('empresa/empresa.html', {'empresa': empresa})
