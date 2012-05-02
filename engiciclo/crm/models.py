963b75037a689354638fbd8f2a1881d1392681e7
Joao Trindade <trindade.joao@gmail.com>
2012-05-02 10:18:51 +0100

    Did some more changes

--- engiciclo/crm/models.py
+++ engiciclo/crm/models.py
@@ -10,14 +10,15 @@ def get_image_path(instance, filename):
         return os.path.join('photos', str(uuid.uuid1()), filename)
 
 class Poster(models.Model):
-    nome_centro = models.CharField('Nome do Centro', max_length=150)
+    nome_centro = models.CharField('Nome Oficial do Centro', max_length=150)
     titulo = models.CharField('Título', max_length=150)
-    financiamento = models.CharField('Financiamento', max_length=150)
-    autores = models.TextField('Autores')
-    descricao_da_foto = models.TextField('Descrição da foto',)
-    resumo = models.TextField('Resumo')
+    entidade_financiadora = models.CharField('Entidade Financiadora', max_length=150)
+    centro_de_investigacao = models.CharField('Centro de Investigação', max_length=150)
+    autores = models.TextField('Autores', max_length=1000)
+    resumo = models.TextField('Resumo', max_length=2000)
 #    data = models.DateTimeField('Data de introducao')
-    poster = models.ImageField('Imagem do poster', upload_to=get_image_path, blank=False)
+    poster = models.ImageField('Imagem', upload_to=get_image_path, blank=False)
+    descricao_da_foto = models.TextField('Descrição da imagem', max_length=2000)
 
     def __unicode__(self):
         return unicode(self.nome_centro)
